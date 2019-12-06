df9bfd0ec733d5e2619348d9af03a5e0562b5234
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-12-06 14:22:26 +0530

    Login db schanges
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- database/__init__.py
+++ database/__init__.py
@@ -1,15 +1,21 @@
-import MySQLdb
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 class UserDb():
 	def __init__(self):
-		self.host = 'localhost'
-		self.user = 'root'
-		self.passwd = '12345'
-		self.db = 'pythonlogin'
-		self.conn = MySQLdb.connect(host= self.host, user = self.user, passwd = self.passwd, db = self.db)
-		cur = self.conn.cursor()
+		self.key = os.urandom(24) #random number
+		self.key = str(key)
+		#secret_key generated
+		self.app.secret_key = key
+		#database connection
+		self.app.config['MYSQL_HOST'] = 'localhost' #changes based on your installation
+		self.app.config['MYSQL_USER'] = 'root'	   #changes based on your installation
+		self.app.config['MYSQL_PASSWORD'] = '12345' #changes based on your installation
+		self.app.config['MYSQL_DB'] = 'pythonlogin'   #changes based on your installation
+		#mysql instance creaion
+		self.mysql = MySQL(app)
 
 	def get_account(self, username, password):
-		cur = self.conn.cursor()
-		cur.execute('SELECT * FROM accounts WHERE username = %s AND password = %s',(username,password))
-		return(cur.fetchone())
+		cursor = self.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s',(username,password))
+		return cursor.fetchone()
